27211002d8270635dbd0acb0646e199ac4b39e0f
robelahamed7374-maker <robelahamed7374@gmail.com>
2026-06-11 12:07:20 +0600

    Refactor script.js for improved functionality and clarity

--- script.js
+++ script.js
@@ -1,81 +1,93 @@
-const saveBtn=document.getElementById("save");
-
-const resetBtn=document.getElementById("reset");
-
 const list=document.getElementById("list");
+const save=document.getElementById("save");
 
-const search=document.querySelector(
-".saved-head input"
-);
+function timeNow(){
+return new Date().toLocaleString();
+}
 
-
-
-
-function load(){
+function draw(){
 
 let data=
-
 JSON.parse(
 localStorage.getItem(
 "rb_system"
-)
-)||[];
+))||[];
 
 list.innerHTML="";
 
-data.forEach((item,index)=>{
+data.forEach((x,i)=>{
 
 list.innerHTML+=`
 
 <div class="card">
 
+<div>
+
 <h2>
-${item.name}
+${x.name}
 </h2>
 
 <br>
 
-<p>
-
+<div>
 Market:
-${item.market}
-
-</p>
+${x.market}
+</div>
 
 <br>
 
-<p>
-
+<div>
 Pair:
-${item.pair}
-
-</p>
+${x.pair}
+</div>
 
 <br>
 
-<p>
-
+<div>
 Win Rate:
-${item.win}
-
-</p>
-
-<br>
-
-<p>
-
-${item.note}
-
-</p>
+${x.win}
+</div>
 
 <br>
+
+<div>
+${x.note}
+</div>
+
+</div>
+
+
+<div class="card-right">
+
+<div class="time">
+${x.time}
+</div>
 
 <button
-onclick="removeItem(${index})">
+class="action play"
+onclick="playVideo('${x.video}')">
 
-Delete
+▶ Play Video
 
 </button>
+
+<button
+class="action photo"
+onclick="viewPhoto()">
+
+🖼 View Photo
+
+</button>
+
+<button
+class="action delete"
+onclick="removeItem(${i})">
+
+🗑 Delete
+
+</button>
+
+</div>
 
 </div>
 
@@ -87,95 +99,62 @@ Delete
 
 
 
-saveBtn.onclick=()=>{
+save.onclick=()=>{
 
-let inputs=
-
+let all=
 document.querySelectorAll(
 "input"
 );
 
 let select=
-
 document.querySelector(
 "select"
 );
 
-let text=
-
+let txt=
 document.querySelector(
 "textarea"
 );
 
-let item={
+let obj={
 
 name:
-inputs[0].value,
+all[0].value,
 
 market:
 select.value,
 
 pair:
-inputs[1].value,
+all[1].value,
 
 win:
-inputs[2].value,
+all[2].value,
 
 note:
-text.value
+txt.value,
+
+video:
+all[3].value,
+
+time:
+timeNow()
 
 };
 
-
-
-if(
-!item.name
-){
-
-alert(
-"Setup Name Required"
-);
-
-return;
-
-}
-
-
-
 let data=
-
 JSON.parse(
 localStorage.getItem(
 "rb_system"
-)
-)||[];
+))||[];
 
-
-data.unshift(item);
-
+data.unshift(obj);
 
 localStorage.setItem(
-
 "rb_system",
-
-JSON.stringify(
-data
-)
-
+JSON.stringify(data)
 );
 
-
-
-inputs.forEach(
-
-x=>x.value=""
-
-);
-
-text.value="";
-
-
-load();
+draw();
 
 };
 
@@ -184,100 +163,50 @@ load();
 function removeItem(i){
 
 let data=
-
 JSON.parse(
-
 localStorage.getItem(
 "rb_system"
-)
-
-)||[];
-
+))||[];
 
 data.splice(i,1);
 
-
 localStorage.setItem(
-
 "rb_system",
-
-JSON.stringify(
-data
-)
-
+JSON.stringify(data)
 );
 
-
-load();
+draw();
 
 }
 
 
 
-resetBtn.onclick=()=>{
+function playVideo(url){
 
-if(
+if(url){
 
-confirm(
-"Delete Everything?"
-)
+window.open(url);
 
-){
+}else{
 
-localStorage.removeItem(
-"rb_system"
+alert(
+"No Video"
 );
-
-load();
 
 }
 
-};
+}
 
 
 
-search.addEventListener(
+function viewPhoto(){
 
-"input",
-
-e=>{
-
-let cards=
-
-document.querySelectorAll(
-".card"
+alert(
+"Photo Preview"
 );
-
-cards.forEach(c=>{
-
-c.style.display=
-
-c.innerText
-
-.toLowerCase()
-
-.includes(
-
-e.target.value
-
-.toLowerCase()
-
-)
-
-?
-
-"block"
-
-:
-
-"none";
-
-});
 
 }
 
-);
 
 
-
-load();
+draw();
